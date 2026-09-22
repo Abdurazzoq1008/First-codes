f92ba1abf3ebd80198a8f0151b3369b38384716b
unknown <saidovabdurazzoq3@gmail.com>
2026-09-22 17:15:58 +0500

    first commit

---
+++ 1.py
@@ -0,0 +1,32 @@
+# print(25+64526)
+# print(7-54620)
+# print(87954*65456632)
+# print(81/9)
+#
+#
+# print(5468%546)
+# print(47%41)
+#
+#
+# print(5**4)
+# print(22%4)
+# print(125*4 , 125**2)
+# print(12/2)
+# print(6**2*3.14)
+# print((6**2+7**2)**0.5)
+
+show1 = "hello tarakan"
+print(show1)
+xabar= 'I play war thunder'
+print(xabar,type(xabar))
+
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# # print("Radiusi", radius ,"ga teng, aylananing yuzi=" , aylana_yuzi)
+#
+# s = "height 1,86cm,"
+# d = "age 17,"
+# f = "name Abdurazzoq."
+# aboutuser = s ,d, f
+# print(aboutuser)
