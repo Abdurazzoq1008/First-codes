91ad28f9918e2de53913e4cb16eecec9f12eb90e
Abdurazzoq1008 <saidovabdurazzoq3@gmail.com>
2026-09-23 19:47:23 +0500

    first commit

---
+++ 3.py
@@ -0,0 +1,24 @@
+# 1
+ism = "Abdurazzoq"
+yosh = 18
+print(ism , yosh )
+# 2
+a = 36
+b = 6
+# yig'indisi
+print(a+b)
+# ayirmasi
+print(a-b)
+# ko'paytmasi
+print(a*b)
+# bo'linmasi'
+print(a/b)
+
+# 3
+uzunligi = 23
+eni = 43
+
+# perimetri
+print(uzunligi * 2 + eni * 2)
+# yuzasi
+print(uzunligi * eni )
